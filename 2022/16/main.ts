75f31e1dd53737ddf9e6c600fa08a4db85ee8fd9
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2024-09-22 20:24:47 +0200

    2022 16 is a hard problem

---
+++ 2022/16/main.ts
@@ -0,0 +1,80 @@
+import { readFile } from '../shared'
+import fs from 'fs';
+
+const day = "16"
+
+
+function SolvePart1(inputFile: string) {
+    let lines = readFile(inputFile).split("\n")
+
+    let valveNames: string[] = []
+    let valves = new Map<string, number>()
+    let tunnels = new Map<string, string[]>()
+
+    for (let line of lines) {
+        let name = line.split(" ")[1]
+        let flow = +line.split("=")[1].split(";")[0]
+        let tun = line.split("lead")[1].substring(" to valves ".length).split(", ")
+
+        valveNames.push(name)
+        valves.set(name, flow)
+        tunnels.set(name, tun)
+    }
+
+    let dists = new Map<string, Map<string, number>>()
+    for (let valve of valveNames) {
+        if (valve != "AA" && valves.get(valve) != 0) {
+            continue
+        }
+
+        dists.set(valve, new Map<string, number>())
+        dists.set(valve, dists.get(valve)!.set(valve, 0))
+        dists.set(valve, dists.get(valve)!.set("AA", 0))
+        let visited: string[] = [valve]
+
+        let queue: [number, string][] = [[0, valve]]
+
+        while (queue.length != 0) {
+            let item = queue.pop()
+            for (let neighbor of tunnels.get(item![1])!) {
+                if (visited.includes(neighbor)) {
+                    continue
+                }
+                visited.push(neighbor)
+                if (valves.get(neighbor) != 0) {
+                    dists.set(valve, dists.get(valve)!.set(neighbor, item![0] + 1))
+                }
+                queue.push([item![0] + 1, neighbor])
+                queue.sort((a, b) => a[0] - b[0])
+            }
+        }
+
+        dists.get(valve)!.delete(valve)
+        if (valve != "AA") {
+            dists.get(valve)!.delete("AA")
+        }
+    }
+
+    console.log(dists)
+
+    return 0
+}
+
+function SolvePart2(inputFile: string) {
+
+    return 0
+}
+
+console.log("== TEST ==")
+console.log(" -Part 1: " + SolvePart1(day + "/a.example"))
+if (fs.existsSync(day + "/b.example")) {
+    console.log(" -Part 2: " + SolvePart2(day + "/b.example"))
+} else {
+    console.log(" -Part 2: " + SolvePart2(day + "/a.example"))
+}
+
+if (false) {
+    console.log("== DATA ==")
+    console.log(" -Part 1: " + SolvePart1(day + "/ab.input"))
+    console.log(" -Part 2: " + SolvePart2(day + "/ab.input"))
+}
--- 2022/README.md
+++ 2022/README.md
@@ -8,3 +8,30 @@ To run your code, run:
 npm start XX/main.ts
 ```
 with XX being the number of the day.
+
+| Day | Status | Comment |
+| 01 | Done |  |
+| 02 | Done |  |
+| 03 | Done |  |
+| 04 | Done |  |
+| 05 | Done |  |
+| 06 | Done |  |
+| 07 | Done |  |
+| 08 | Done |  |
+| 09 | Done |  |
+| 10 | Done |  |
+| 11 | Done |  |
+| 12 | Done |  |
+| 13 | Done |  |
+| 14 | Done |  |
+| 15 | Done |  |
+| 16 | Started |  |
+| 17 |  |  |
+| 18 |  |  |
+| 19 |  |  |
+| 20 |  |  |
+| 21 |  |  |
+| 22 |  |  |
+| 23 |  |  |
+| 24 |  |  |
+| 25 |  |  |
