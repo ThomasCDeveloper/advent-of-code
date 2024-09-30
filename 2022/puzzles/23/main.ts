c79f6f30fb058e2e76de12cf280acab3e02a3641
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2024-09-30 22:48:29 +0200

    some work on 2022 23

--- 2022/puzzles/23/main.ts
+++ 2022/puzzles/23/main.ts
@@ -4,16 +4,27 @@ import fs from 'fs';
 const day = "23"
 
 function SolvePart1(inputFile: string) {
-    let compt = 0
-    readFile(inputFile).split("\n").forEach((line) => {
-        line.split("").forEach((char) => {
-            if (char == "#") compt++
-        })
-    })
+    let monkeys: [number, number][] = []
 
-    console.log(compt)
+    let lines = readFile(inputFile).split("\n")
+    for (let y = 0; y < lines.length; y++) {
+        for (let x = 0; x < lines[y].length; x++) {
+            if (lines[y][x] == "#") {
+                monkeys.push([x, y])
+            }
+        }
+    }
 
-    return 0
+    for (let i = 0; i < 10; i++) {
+        // do stuff
+    }
+
+    let maxX = Math.max(...monkeys.map((m) => m[0]))
+    let maxY = Math.max(...monkeys.map((m) => m[1]))
+    let minX = Math.min(...monkeys.map((m) => m[0]))
+    let minY = Math.min(...monkeys.map((m) => m[1]))
+
+    return (maxX - minX + 1) * (maxY - minY + 1) - monkeys.length
 }
 
 function SolvePart2(inputFile: string) {
@@ -29,7 +40,7 @@ if (fs.existsSync(day + "/b.example")) {
     console.log(" -Part 2: " + SolvePart2(day + "/a.example"))
 }
 
-if (true) {
+if (false) {
     console.log("== DATA ==")
     console.log(" -Part 1: " + SolvePart1(day + "/ab.input"))
     console.log(" -Part 2: " + SolvePart2(day + "/ab.input"))
