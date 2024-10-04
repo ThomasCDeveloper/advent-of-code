c49aca5fbdb68659e161de60e3f2994da16980dc
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2024-10-04 21:32:38 +0200

    2022 24

--- 2022/puzzles/24/main.ts
+++ 2022/puzzles/24/main.ts
@@ -3,12 +3,6 @@ import fs from 'fs';
 
 const day = "24"
 
-const isStatusSame = (a: [number, number, number], b: [number, number, number]) =>
-    a[0] === b[0] && a[1] === b[1] && a[2] === b[2]
-
-const isPositionSame = (a: [number, number], b: [number, number]) =>
-    a[0] === b[0] && a[1] === b[1]
-
 const gcd = (a: number, b: number): number => {
     const r = a % b
 
@@ -21,59 +15,47 @@ const gcd = (a: number, b: number): number => {
 
 function SolvePart1(inputFile: string) {
     let lines = readFile(inputFile).split("\n")
-    let map: string[][] = []
-    for (let y = 0; y < lines.length - 2; y++) {
-        map.push([])
-        for (let x = 1; x < lines[y + 1].length - 1; x++) {
-            map[y].push(lines[y + 1][x])
-        }
-    }
+    let blizzards: Set<string>[] = Array.from({ length: 4 }, () => new Set()) // ><^v
 
-    let loop = map.length * map[0].length / gcd(map.length, map[0].length)
-
-    let directions = [
-        [map[0].length - 1, 0],
-        [1, 0],
-        [0, 1],
-        [0, map.length - 1],
-    ]
-
-    let blizzards: [number, number][][] = [[], [], [], []] // ><^v
-
-    for (let y = 0; y < map.length; y++) {
-        for (let x = 0; x < map[y].length; x++) {
-            let index = "><^v".indexOf(map[y][x])
+    for (let y = 1; y < lines.length; y++) {
+        for (let x = 1; x < lines[y].length; x++) {
+            let index = "><^v".indexOf(lines[y][x])
             if (index >= 0) {
-                blizzards[index].push([x, y])
+                blizzards[index].add(`${x - 1},${y - 1}`)
             }
         }
     }
 
+    const [mx, my] = [lines[0].length - 2, lines.length - 2]
+
+    let loop = mx * my / gcd(mx, my)
+
+    let directions = [
+        [mx - 1, 0],
+        [1, 0],
+        [0, 1],
+        [0, my - 1],
+    ]
+
     let queue: [number, number, number][] = [[0, -1, 0]] // x, y, time
-    let visited: [number, number, number][] = []
+    let visited = new Set<string>()
 
     while (queue.length != 0) {
         let status = queue.shift()!
 
-        for (let [x, y] of [[status[0], status[1]], [status[0] + 1, status[1]], [status[0] - 1, status[1]], [status[0], status[1] + 1], [status[0], status[1] - 1]]) {
-            if (x == map[0].length - 1 && y == map.length) {
+        for (let [dx, dy] of [[0, 0], [1, 0], [-1, 0], [0, -1], [0, 1]]) {
+            const [x, y] = [status[0] + dx, status[1] + dy]
+
+            if (x == mx - 1 && y == my) {
                 return status[2]
             }
-            if ((x < 0 || x >= map[0].length || y < 0 || y >= map.length) && (x != 0 || y != -1)) {
+            if ((x < 0 || x >= mx || y < 0 || y >= my) && (x != 0 || y != -1)) {
                 continue
             }
 
-            let nextStatus: [number, number, number] = [x, y, (status[2] + 1)]
-
-            if (visited.some(v => isStatusSame(v, [nextStatus[0], nextStatus[1], nextStatus[2] % loop])) || queue.some(q => isStatusSame(q, [nextStatus[0], nextStatus[1], nextStatus[2] % loop]))) {
-                continue
-            }
-
-            // left to check if player is on a blizzard
-            // think of a way to move player instead of 654615654 blizzards
             let isOnBlizzard = false
             for (let i = 0; i < 4; i++) {
-                if (blizzards[i].some(b => isPositionSame(b, [(x + status[2] * directions[i][0]) % map[0].length, (y + status[2] * directions[i][1]) % map.length]))) {
+                if (blizzards[i].has(`${(x + status[2] * directions[i][0]) % mx},${(y + status[2] * directions[i][1]) % my}`)) {
                     isOnBlizzard = true
                     break
                 }
@@ -82,20 +64,95 @@ function SolvePart1(inputFile: string) {
                 continue
             }
 
+            let nextStatus: [number, number, number] = [x, y, status[2] + 1]
+            const key = `${x},${y},${nextStatus[2] % loop}`
+
+            if (visited.has(key)) {
+                continue
+            }
+
             queue.push(nextStatus)
-            visited.push([nextStatus[0], nextStatus[1], nextStatus[2] % loop])
+            visited.add(key)
         }
 
-
         queue.sort((a, b) => a[2] - b[2])
-        console.log(queue.length)
     }
-
     return 0
 }
 
 function SolvePart2(inputFile: string) {
+    let lines = readFile(inputFile).split("\n")
+    let blizzards: Set<string>[] = Array.from({ length: 4 }, () => new Set()) // ><^v
 
+    for (let y = 1; y < lines.length; y++) {
+        for (let x = 1; x < lines[y].length; x++) {
+            let index = "><^v".indexOf(lines[y][x])
+            if (index >= 0) {
+                blizzards[index].add(`${x - 1},${y - 1}`)
+            }
+        }
+    }
+
+    const [mx, my] = [lines[0].length - 2, lines.length - 2]
+
+    let loop = mx * my / gcd(mx, my)
+
+    let directions = [
+        [mx - 1, 0],
+        [1, 0],
+        [0, 1],
+        [0, my - 1],
+    ]
+
+    let targets = [
+        [mx - 1, my],
+        [0, -1]
+    ]
+
+    let queue: [number, number, number, number][] = [[0, -1, 0, 0]] // x, y, time, step
+    let visited = new Set<string>()
+
+    while (queue.length != 0) {
+        let [ox, oy, time, step] = queue.shift()!
+        time += 1
+
+        for (let [dx, dy] of [[0, 0], [1, 0], [-1, 0], [0, -1], [0, 1]]) {
+            const [x, y] = [ox + dx, oy + dy]
+
+            let nstep = step
+            if (x == targets[step % 2][0] && y == targets[step % 2][1]) {
+                if (step == 2)
+                    return time
+                nstep++
+            }
+            if ((x < 0 || x >= mx || y < 0 || y >= my) && ((x != 0 || y != -1) && (x != mx - 1 || y != my))) {
+                continue
+            }
+
+            let isOnBlizzard = false
+            if (((x != 0 || y != -1) && (x != mx - 1 || y != my)))
+                for (let i = 0; i < 4; i++) {
+                    if (blizzards[i].has(`${(x + time * directions[i][0]) % mx},${(y + time * directions[i][1]) % my}`)) {
+                        isOnBlizzard = true
+                        break
+                    }
+                }
+            if (isOnBlizzard) {
+                continue
+            }
+
+            const key = `${x},${y},${time % loop},${step}`
+
+            if (visited.has(key)) {
+                continue
+            }
+
+            queue.push([x, y, time, nstep])
+            visited.add(key)
+        }
+
+        queue.sort((a, b) => a[2] - b[2])
+    }
     return 0
 }
 
