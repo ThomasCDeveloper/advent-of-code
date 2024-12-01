827e825379a9f07c00119273a870a94f34edcf0b
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2024-12-01 18:35:10 +0100

    Cleaned 2024/01 with GPT suggestions

--- 2024/01/main.ml
+++ 2024/01/main.ml
@@ -14,44 +14,27 @@ let read_file filename =
       close_in_noerr ic;
       raise e
 
-let rec print_list list =
-  List.iter (fun x -> print_string ((string_of_int x) ^ "\n")) list;
-  print_newline ()
-
 let extract_lefts lines len =
-  let rec extract_lefts_acc acc = function
-    | [] -> acc
-    | x :: xs ->
-        let left = int_of_string (String.sub x 0 len) in
-        extract_lefts_acc (left :: acc) xs
-  in
-  extract_lefts_acc [] lines
+  List.map (fun x -> int_of_string (String.sub x 0 len)) lines
 
 let extract_rights lines len =
-  let rec extract_rights_acc acc = function
-    | [] -> acc
-    | x :: xs ->
-        let right = int_of_string (String.sub x (len+3) len) in
-        extract_rights_acc (right :: acc) xs
-  in
-  extract_rights_acc [] lines
+  List.map (fun x -> int_of_string (String.sub x (len + 3) len)) lines
 
 let part1 lefts rights =
   let differences = List.map2 (fun left right -> abs (left - right)) lefts rights in
   List.fold_left ( + ) 0 differences
 
-let count_occurences number list =
-  List.fold_left (fun acc x -> if x = number then acc + 1 else acc) 0 list
+let count_occurences number lst =
+  List.fold_left (fun acc x -> if x = number then acc + 1 else acc) 0 lst
 
 let part2 lefts rights =
-  List.fold_left(fun similarity element -> similarity + element * count_occurences element rights) 0 lefts
+  List.fold_left (fun similarity element -> similarity + element * count_occurences element rights) 0 lefts
 
 let filename = "input.txt"
 let length =
   match filename with
   | "input.txt" -> 5
   | _ -> 1
-
 
 let () =
   let lines = read_file filename in
