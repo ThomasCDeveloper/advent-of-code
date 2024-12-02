8b7bef3755a9ffa9d1b6b9cfdc1f0196d912a8ae
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2024-12-02 19:38:56 +0100

    Changed default file for Ocaml

--- 2024/bin/day00.ml
+++ 2024/bin/day00.ml
@@ -1,47 +1,13 @@
-let read_file filename =
-  let ic = open_in filename in
-  try
-    let rec read_lines acc =
-      match input_line ic with
-      | line -> read_lines (line :: acc)
-      | exception End_of_file -> acc
-    in
-    let lines = read_lines [] in
-    close_in ic;
-    lines
-  with e ->
-    close_in_noerr ic;
-    raise e
+open Base
+open Stdio
 
-let extract_lefts lines len =
-  List.map (fun x -> int_of_string (String.sub x 0 len)) lines
-
-let extract_rights lines len =
-  List.map (fun x -> int_of_string (String.sub x (len + 3) len)) lines
-
-let part1 lefts rights =
-  let differences =
-    List.map2 (fun left right -> abs (left - right)) lefts rights
-  in
-  List.fold_left ( + ) 0 differences
-
-let count_occurences number lst =
-  List.fold_left (fun acc x -> if x = number then acc + 1 else acc) 0 lst
-
-let part2 lefts rights =
-  List.fold_left
-    (fun similarity element ->
-      similarity + (element * count_occurences element rights))
-    0 lefts
-
-let filename = "input.txt"
-let length = match filename with "input.txt" -> 5 | _ -> 1
+let read_file filename = In_channel.read_all filename
+let part1 lines = 0
+let part2 lines = 0
+let filename = "input/day00.tst"
 
 let () =
-  let lines = read_file filename in
-  let lefts = List.sort Int.compare (extract_lefts lines length) in
-  let rights = List.sort Int.compare (extract_rights lines length) in
-  print_string
-    ("Solution to part 1: " ^ string_of_int (part1 lefts rights) ^ "\n");
-  print_string
-    ("Solution to part 2: " ^ string_of_int (part2 lefts rights) ^ "\n")
+  let content = In_channel.read_all filename in
+  let lines = String.split_lines content in
+  print_string ("Solution to part 1: " ^ Int.to_string (part1 lines) ^ "\n");
+  print_string ("Solution to part 2: " ^ Int.to_string (part2 lines) ^ "\n")
