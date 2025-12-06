eeb806f4b6acfb95168d1c40123644e2e45fda53
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2025-12-06 18:25:39 +0100

    2023 02

--- 2023-Go/solutions/02/solution.go
+++ 2023-Go/solutions/02/solution.go
@@ -4,14 +4,63 @@ import (
 	"ThomasCDeveloper/advent-of-code/2023/utils"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func part1(data []string) int {
-	return 0
+	total := 0
+
+	for _, game := range data {
+		possible := true
+		id := utils.Atoi(strings.Split(strings.Split(game, ": ")[0], " ")[1])
+
+		sets := strings.Split(strings.Split(game, ": ")[1], "; ")
+		for _, set := range sets {
+			for _, color := range strings.Split(set, ", ") {
+				num := strings.Split(color, " ")[0]
+				word := strings.Split(color, " ")[1]
+
+				if word == "red" && utils.Atoi(num) > 12 {
+					possible = false
+				}
+				if word == "green" && utils.Atoi(num) > 13 {
+					possible = false
+				}
+				if word == "blue" && utils.Atoi(num) > 14 {
+					possible = false
+				}
+			}
+		}
+		if possible {
+			total += id
+		}
+
+	}
+
+	return total
 }
 
 func part2(data []string) int {
-	return 0
+	total := 0
+	for _, game := range data {
+		minimums := map[string]int{}
+
+		sets := strings.Split(strings.Split(game, ": ")[1], "; ")
+		for _, set := range sets {
+			for _, color := range strings.Split(set, ", ") {
+				num := utils.Atoi(strings.Split(color, " ")[0])
+				word := strings.Split(color, " ")[1]
+
+				if val, ok := minimums[word]; ok {
+					minimums[word] = max(val, num)
+				} else {
+					minimums[word] = num
+				}
+			}
+		}
+		total += minimums["green"] * minimums["blue"] * minimums["red"]
+	}
+	return total
 }
 
 func main() {
