cd235c1ed3d32b0411553ac934016cf428f10d08
ThomasCDeveloper <thomas.chaumont.pro@gmail.com>
2025-12-08 13:06:38 +0100

    2025 02 imp

--- 2025-Python/solutions/02/solution.py
+++ 2025-Python/solutions/02/solution.py
@@ -11,7 +11,8 @@ def check_num(num):
     return s[: n // 2] == s[n // 2 :]
 
 
-def part1(ranges):
+def part1(data):
+    ranges = [list(map(int, x.split("-"))) for x in data.split(",")]
     total = 0
     for low, high in ranges:
         for i in range(low, high + 1):
@@ -22,33 +23,51 @@ def part1(ranges):
     return total
 
 
-def check_num2(num):
-    s = str(num)
-    n = len(s)
-    for i in range(1, n // 2 + 1):
-        if n % i == 0 and s == s[:i] * (n // i):
-            return True
-    return False
+def part2(data):
+    ranges = [tuple(map(int, x.split("-"))) for x in data.split(",")]
 
+    global_low = min(low for low, _ in ranges)
+    global_high = max(high for _, high in ranges)
+    max_digits = len(str(global_high))
 
-def part2(ranges):
-    total = 0
-    for low, high in ranges:
-        for i in range(low, high + 1):
-            s = str(i)
-            n = len(s)
-            for k in range(1, n // 2 + 1):
-                if n % k == 0 and s == s[:k] * (n // k):
-                    total += i
+    repeating_numbers = set()
+
+    for n in range(2, max_digits + 1):
+        for k in range(1, n // 2 + 1):
+            if n % k != 0:
+                continue
+
+            repeat_count = n // k
+            if repeat_count < 2:
+                continue
+
+            start_block = 10 ** (k - 1)
+            end_block = 10**k
+
+            for block in range(start_block, end_block):
+                s = str(block) * repeat_count
+                if len(s) != n:
+                    continue
+
+                value = int(s)
+                if value > global_high:
                     break
+                if value >= global_low:
+                    repeating_numbers.add(value)
+
+    total = 0
+    for value in repeating_numbers:
+        for low, high in ranges:
+            if low <= value <= high:
+                total += value
+                break
+
     return total
 
 
 if __name__ == "__main__":
     BASE = Path(__file__).resolve().parent
     file_path = BASE / utils.get_mode(sys.argv)
-    data = [
-        list(map(int, x.split("-"))) for x in utils.parse_input(file_path).split(",")
-    ]
+    data = utils.parse_input(file_path)
     print("Part 1:", part1(data))
     print("Part 2:", part2(data))
